87e66d25b73e2a37761bab64928e6be211fde123
Delofon <spacedelofon@gmail.com>
2026-03-16 01:46:25 +0300

    vec2 code deduplication

--- vec.h
+++ vec.h
@@ -1,9 +1,7 @@
 #ifndef VEC_H_
 #define VEC_H_
 
-#ifdef SI_VEC
 #include <math.h>
-#endif
 
 #include "def.h"
 
@@ -13,13 +11,13 @@ typedef struct
     scalar_t y;
 } vec2;
 
-extern const vec2 v2_z;
+//extern const vec2 v2_z;
+#define v2_z ((vec2){ 0.0, 0.0 })
 
 // for some reason, debug builds with G*m/r2 acc
 // (as opposed to G*m/r) are slower with static inline
 // than without...
 #ifndef SI_VEC
-
 vec2 *vec2_addeq(vec2 *restrict a, const vec2 *restrict b);
 vec2 *vec2_subeq(vec2 *restrict a, const vec2 *restrict b);
 vec2 *vec2_vsmuleq(vec2 *a, const scalar_t b);
@@ -27,31 +25,38 @@ vec2 *vec2_vvmuleq(vec2 *restrict a, const vec2 *restrict b);
 
 scalar_t vec2_magsqr(const vec2 *vec);
 scalar_t vec2_mag(const vec2 *vec);
+#endif
 
+#ifdef SI_VEC
+#define SIVEC static inline
 #else
+#define SIVEC
+#endif
 
-static inline vec2 *vec2_addeq(vec2 *restrict a, const vec2 *restrict b)
+#if defined(SI_VEC) || defined(VEC_IMPLEMENTATION)
+
+SIVEC vec2 *vec2_addeq(vec2 *restrict a, const vec2 *restrict b)
 {
     a->x += b->x;
     a->y += b->y;
 
     return a;
 }
-static inline vec2 *vec2_subeq(vec2 *restrict a, const vec2 *restrict b)
+SIVEC vec2 *vec2_subeq(vec2 *restrict a, const vec2 *restrict b)
 {
     a->x -= b->x;
     a->y -= b->y;
 
     return a;
 }
-static inline vec2 *vec2_vsmuleq(vec2 *a, const scalar_t b)
+SIVEC vec2 *vec2_vsmuleq(vec2 *a, const scalar_t b)
 {
     a->x *= b;
     a->y *= b;
 
     return a;
 }
-static inline vec2 *vec2_vvmuleq(vec2 *restrict a, const vec2 *restrict b)
+SIVEC vec2 *vec2_vvmuleq(vec2 *restrict a, const vec2 *restrict b)
 {
     a->x *= b->x;
     a->y *= b->y;
@@ -59,16 +64,16 @@ static inline vec2 *vec2_vvmuleq(vec2 *restrict a, const vec2 *restrict b)
     return a;
 }
 
-static inline scalar_t vec2_magsqr(const vec2 *vec)
+SIVEC scalar_t vec2_magsqr(const vec2 *vec)
 {
     return vec->x*vec->x + vec->y*vec->y;
 }
-static inline scalar_t vec2_mag(const vec2 *vec)
+SIVEC scalar_t vec2_mag(const vec2 *vec)
 {
     return sqrt(vec->x*vec->x + vec->y*vec->y);
 }
 
-#endif // SI_VEC
+#endif // if defined
 
 #endif
 
